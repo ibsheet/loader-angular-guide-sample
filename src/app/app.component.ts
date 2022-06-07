cf48f8cebbda1c0d53abf92d05b6532ba2b831de
이재호 <jhlee@softin.co.kr>
2022-06-07 11:20:02 +0900

    Update: loader 초기화 수정

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
 import loader from '@ibsheet/loader';
 
+const ibsheetLib = {
+  name: 'ibsheet',
+  baseUrl: 'https://demo.ibleaders.com/ibsheet/v8/samples/customer-sample/assets/ibsheet/',
+  locales: ['en', 'ko'],
+  plugins: ['excel', 'common', 'dialog']
+}
+
+// 로더 config
 loader.config({
-  registry: {
-    name: 'ibsheet',
-    baseUrl: '/assets/ibsheet'
+  registry: [ibsheetLib]
+});
+
+// 로더 이벤트
+loader.once('loaded', (evt) => {
+  const target = evt.target;
+  if (target.alias === ibsheetLib.name) {
+    let IBSheet = loader.getIBSheetStatic();
+    console.log('loaded', IBSheet);
   }
 });
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
